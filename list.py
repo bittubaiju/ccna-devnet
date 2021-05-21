981a4a248288b794cf6d9f6dc65b83d52b17aa81
bittu <bittubaiju2756@gmail.com>
2021-05-21 23:51:22 +0530

    added tuples

--- list.py
+++ list.py
@@ -7,4 +7,9 @@ print(a)
 print(a.index(3))
 print(a.pop(4)) #removes the given value from the list
 print(a.remove(1)) #removes the given value and returns none. arguement taken value not index
-print(a)
+print(type(a))
+#TUPLES
+tpls = ('bittu','achu','amma') #tuples are immutable so if we have some data which we dont tochangethentupleistheoption
+print(tpls[0])
+print(type(tpls))
+#DICTIONARY
