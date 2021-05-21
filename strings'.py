d57deacf24c2dd48a962f55bacd868be0055695b
bittu <bittubaiju2756@gmail.com>
2021-05-21 23:17:42 +0530

    added some sting functions.

--- strings'.py
+++ strings'.py
@@ -9,5 +9,6 @@ print(b[1:])
 print(a[-1]) # will print nothing and also will not throw any error.
 print(a[:-3]) # it will print from starting up to 1 less than -3 which in this case is t from BEHIND.
 print(a[-2:]) # will start from last as it is negative index and will go till the end. so in this it will print "u"
-
+print(a.capitalize()) #only first letter
+print(b.upper()) # will make whole word capital
 
