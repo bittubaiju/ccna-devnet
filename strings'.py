c83263304b8ac232d6c154c45cb4f9ce04c3257d
bittu <bittubaiju2756@gmail.com>
2021-05-21 23:03:08 +0530

    some more strings.

--- strings'.py
+++ strings'.py
@@ -3,4 +3,11 @@ b = 'baiju'
 print(a + b)
 # print(a*b) # will throw error as string can only be multiplied with int
 print(a[:2]+b[3:]) # [a:b] will not include b
-print(a[:-2])
+print(a[1:-2])
+print(b[-1:])
+print(b[1:])
+print(a[-1]) # will print nothing and also will not throw any error.
+print(a[:-3]) # it will print from starting up to 1 less than -3 which in this case is t from BEHIND.
+print(a[-2:]) # will start from last as it is negative index and will go till the end. so in this it will print "u"
+
+
