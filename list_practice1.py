a554a97e395057c6973788451769a359e58099d1
bittu <bittubaiju2756@gmail.com>
2021-05-22 21:28:24 +0530

    list practice1

---
+++ list_practice1.py
@@ -0,0 +1,9 @@
+num = [10,20,30,40]
+print('total list value: ',num)
+print('first= %d,last= %d'%(num[0],num[3])) #%d format specifier for integer
+name = ['Bittu','Achu','Amma','Acha']
+print('Total names:',name)
+print('First name: %s, Last name: %s'%(name[0],name[3])) #format specifier for string
+marks = [12.4,45.6,67.3,76.8]
+print('total marks:', marks)
+print('first mark: %2.1f, last mark:%.1f'%(marks[0],marks[3]))# %2.1f here 2 means space and 1 means decimal after int
