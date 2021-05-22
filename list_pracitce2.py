c1f40c6f9708f7c01f838b60bf325da18a167730
bittu <bittubaiju2756@gmail.com>
2021-05-22 21:28:12 +0530

    list practice

---
+++ list_pracitce2.py
@@ -0,0 +1,9 @@
+lst1 = [10,20,30,40,50,60]
+i = 0
+print(len(lst1))
+while i < len(lst1): # will go up to n-1 value which in this case is 6-1=5
+    print(lst1[i])
+    i += 1
+print('with for loop')
+for j in lst1:
+    print(j)
