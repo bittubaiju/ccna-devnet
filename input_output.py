019edf8482039e5fef5f88d3b5be3894ef2cfe18
bittu <bittubaiju2756@gmail.com>
2021-05-22 13:31:15 +0530

    adding input output file

---
+++ input_output.py
@@ -0,0 +1,6 @@
+inpt = input("Enter the name: ")
+print(inpt)
+print(type(inpt)) # By default type of input will be string
+marks = float(input("enter the marks: "))
+print(marks,inpt,sep=':') #by default sep is space.
+# fstring, dont know the functionality.
