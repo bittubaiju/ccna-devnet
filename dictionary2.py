8ca39321bc638ca6cbc087fbb01ac062863be1aa
bittu <bittubaiju2756@gmail.com>
2021-05-25 20:54:19 +0530

    dictionary functions

---
+++ dictionary2.py
@@ -0,0 +1,6 @@
+# program to print keys, value and then key value
+dict = {'name':'Bittu','age':24,'gender':'Male'}
+print(dict)
+print('keys in dict: ',dict.keys())  #will print only keys, return: tuple of list
+print('value in dict: ',dict.values()) #will print only value, return: tuple of list
+print('key value combinedare: ',dict.items())#will print whole dict,return:tuple of list where each key:value is a tuple
